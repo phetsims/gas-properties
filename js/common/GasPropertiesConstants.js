e672c1b012dedb5ac776083364786bff5472c3a1
Chris Malley <cmalley@pixelzoom.com>
2018-07-16 12:45:13 -0600

    constants and query parameters
    Signed-off-by: Chris Malley <cmalley@pixelzoom.com>

---
+++ js/common/GasPropertiesConstants.js
@@ -0,0 +1,21 @@
+// Copyright 2018, University of Colorado Boulder
+
+/**
+ * Constants used throughout this simulation.
+ *
+ * @author Chris Malley (PixelZoom, Inc.)
+ */
+define( function( require ) {
+  'use strict';
+
+  // modules
+  var gasProperties = require( 'GAS_PROPERTIES/gasProperties' );
+
+  var GasPropertiesConstants = {
+    //TODO
+  };
+
+  gasProperties.register( 'GasPropertiesConstants', GasPropertiesConstants );
+
+  return GasPropertiesConstants;
+} );
---
+++ js/common/GasPropertiesQueryParameters.js
@@ -0,0 +1,29 @@
+// Copyright 2018, University of Colorado Boulder
+
+/**
+ * Query parameters that are specific to the Equality Explorer sim.
+ *
+ * Running with ?log will print these query parameters and their values to the console.
+ *
+ * Running with ?dev shows the following things that are specific to this sim:
+ * TODO
+ *
+ * @author Chris Malley (PixelZoom, Inc.)
+ */
+define( function( require ) {
+  'use strict';
+
+  // modules
+  var gasProperties = require( 'GAS_PROPERTIES/gasProperties' );
+
+  var GasPropertiesQueryParameters = QueryStringMachine.getAll( {
+    //TODO
+  } );
+
+  gasProperties.register( 'GasPropertiesQueryParameters', GasPropertiesQueryParameters );
+
+  // log the values of all sim-specific query parameters
+  phet.log && phet.log( 'query parameters: ' + JSON.stringify( GasPropertiesQueryParameters, null, 2 ) );
+
+  return GasPropertiesQueryParameters;
+} );
