c7a8562c8493124df1c7b4e9a916d6a8d551e4c0
Chris Malley <cmalley@pixelzoom.com>
2018-11-13 16:08:27 -0700

    use NumberDisplay to right-align temperature values
    Signed-off-by: Chris Malley <cmalley@pixelzoom.com>

--- js/common/view/TemperatureComboBox.js
+++ js/common/view/TemperatureComboBox.js
@@ -12,10 +12,10 @@ define( require => {
   const ComboBox = require( 'SUN/ComboBox' );
   const DerivedProperty = require( 'AXON/DerivedProperty' );
   const gasProperties = require( 'GAS_PROPERTIES/gasProperties' );
+  const NumberDisplay = require( 'SCENERY_PHET/NumberDisplay' );
   const PhetFont = require( 'SCENERY_PHET/PhetFont' );
-  const Text = require( 'SCENERY/nodes/Text' );
+  const Range = require( 'DOT/Range' );
   const StringUtils = require( 'PHETCOMMON/util/StringUtils' );
-  const Util = require( 'DOT/Util' );
 
   // strings
   var celsiusString = require( 'string!GAS_PROPERTIES/celsius' );
@@ -23,26 +23,24 @@ define( require => {
   var temperatureUnitsString = require( 'string!GAS_PROPERTIES/temperatureUnits' );
 
   // constants
-  var FONT = new PhetFont( 12 );
-  var MAX_TEMPERATURE = 9999;
-  var MAX_TEMPERATURE_KELVIN = StringUtils.fillIn( temperatureUnitsString, {
-    temperature: MAX_TEMPERATURE,
-    units: kelvinString
-  } );
-  var MAX_TEMPERATURE_CELSIUS = StringUtils.fillIn( temperatureUnitsString, {
-    temperature: MAX_TEMPERATURE,
-    units: celsiusString
-  } );
+  const NUMBER_DISPLAY_RANGE = new Range( -9999, 9999 );
+  const NUMBER_DISPLAY_OPTIONS = {
+    backgroundStroke: null,
+    font: new PhetFont( 12 ),
+    align: 'right',
+    xMargin: 0,
+    yMargin: 0
+  };
 
   class TemperatureComboBox extends ComboBox {
 
     /**
-     * @param {NumberProperty} temperatureProperty
+     * @param {NumberProperty} temperatureKelvinProperty
      * @param {StringProperty} temperatureUnitsProperty
      * @param {Node} listParent - parent for the combo box list
      * @param {Object} [options]
      */
-    constructor( temperatureProperty, temperatureUnitsProperty, listParent, options ) {
+    constructor( temperatureKelvinProperty, temperatureUnitsProperty, listParent, options ) {
 
       options = _.extend( {
         buttonXMargin: 5,
@@ -53,40 +51,40 @@ define( require => {
         buttonLineWidth: 0.4
       }, options );
 
-      //TODO use NumberDisplay with align:'right'
-      // Nodes for combo box items
-      const kelvinNode = new Text( MAX_TEMPERATURE_KELVIN, { font: FONT } );
-      const celsiusNode = new Text( MAX_TEMPERATURE_CELSIUS, { font: FONT } );
+      // displays the temperature in K
+      const kelvinNode = new NumberDisplay( temperatureKelvinProperty, NUMBER_DISPLAY_RANGE,
+        _.extend( {}, NUMBER_DISPLAY_OPTIONS, {
+          valuePattern: StringUtils.fillIn( temperatureUnitsString, {
+            temperature: '{0}',
+            units: kelvinString
+          } )
+        } ) );
 
-      // Set the same maxWidth for both items, since we'll be changing their text.
+      // temperature in C
+      const temperatureCelsiusProperty = new DerivedProperty( [ temperatureKelvinProperty ],
+        temperature => temperature - 273.15 );
+
+      // displays the temperature in C
+      const celsiusNode = new NumberDisplay( temperatureCelsiusProperty, NUMBER_DISPLAY_RANGE,
+        _.extend( {}, NUMBER_DISPLAY_OPTIONS, {
+          valuePattern: StringUtils.fillIn( temperatureUnitsString, {
+            temperature: '{0}',
+            units: celsiusString
+          } )
+        } ) );
+
+      // Set the same maxWidth for both item Nodes, since their values will change dynamically.
       const maxWidth = Math.max( kelvinNode.width, celsiusNode.width );
       kelvinNode.maxWidth = maxWidth;
       celsiusNode.maxWidth = maxWidth;
 
+      // Items to be displayed in the combo box
       const items = [
         ComboBox.createItem( kelvinNode, 'kelvin' ),
         ComboBox.createItem( celsiusNode, 'celsius' )
       ];
 
       super( items, temperatureUnitsProperty, listParent, options );
-
-      // update temperature in K
-      temperatureProperty.link( temperature => {
-        kelvinNode.text = StringUtils.fillIn( temperatureUnitsString, {
-          temperature: Util.roundSymmetric( temperature ),
-          units: kelvinString
-        } );
-      } );
-
-      // update temperature in C
-      const temperatureCelsiusProperty = new DerivedProperty( [ temperatureProperty ],
-        temperature => temperature - 273.15 );
-      temperatureCelsiusProperty.link( temperature => {
-        celsiusNode.text = StringUtils.fillIn( temperatureUnitsString, {
-          temperature: Util.roundSymmetric( temperature ),
-          units: celsiusString
-        } );
-      } );
     }
   }
 
