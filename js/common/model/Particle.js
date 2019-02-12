ac57a43e6e4332630ef004a9eb892b22c68ae6c4
Chris Malley <cmalley@pixelzoom.com>
2019-02-12 15:19:05 -0700

    delete unneeded stuff from Java model

--- js/common/model/Particle.js
+++ js/common/model/Particle.js
@@ -1,7 +1,7 @@
 // Copyright 2019, University of Colorado Boulder
 
 //TODO #37 JAVA hierarchy is SimpleObservable, ModelElement > Particle > Body > CollidableBody > SphericalBody > SolidSphere > GasMolecule > HeavySpecies, LightSpecies
-//TODO optimize by making all Vector2 herein mutable
+//TODO optimize by making all Vector2 herein mutable?
 /**
  * Model for all types of particles.
  *
@@ -28,45 +28,27 @@ define( require => {
         location: Vector2.ZERO,
         velocity: Vector2.ZERO,
         acceleration: Vector2.ZERO,
-
-        //TODO should mass be in kg, since other quantities are in kg?
         mass: 1, // u, atomic mass unit, 1 u === 1.66 x 10-27 kg
         radius: 1,
         colorProperty: null
       }, options );
 
-      //JAVA from java.Particle
+      // @public (read-only)
       this.location = options.location; // {Vector2} m
       this.velocity = options.velocity; // {Vector2} m/s
       this.acceleration = options.acceleration; // {Vector2} m/s^2
-      this.previousAcceleration = this.acceleration; // {Vector2} m/s^2
-
-      //JAVA from java.Body
       this.momentum = Vector2.ZERO; // kg * m/s
       this.mass = options.mass; // u
-      this.lastCollidedBody = null;  //TODO what is this? Body? Particle?
-
-      //JAVA from java.CollidableBody
-      this.collidable = true;
-      this.containedBodies = []; //TODO what is this? Body[]? Particle[]?
-      this.constraints = []; // {Constraint[]} constraints applied to the particle's state at the end of each step
-      this.previousLocation = null;
-      this.previousVelocity = null;
-
-      //JAVA from java.SphericalBody
       this.radius = options.radius; // m
-
       this.colorProperty = options.colorProperty || new Property( 'white' );
-
-      // @public emit is called with no args when Particle has been disposed.
-      this.disposedEmitter = new Emitter();
+      this.disposedEmitter = new Emitter(); // emit is called with no args when Particle has been disposed.
       this.isDisposed = false;
     }
 
     dispose() {
       assert && assert( !this.isDisposed, 'attempted to dispose again' );
-      this.disposedEmitter.emit();
       this.isDisposed = true;
+      this.disposedEmitter.emit();
     }
 
     /**
@@ -74,6 +56,8 @@ define( require => {
      * @returns {number}
      */
     getSpeed() { this.velocity.magnitude(); }
+
+    get speed() { return this.getSpeed(); }
 
     /**
      * Gets the particle's kinetic energy. This is due to translation only, no rotation.
@@ -83,6 +67,8 @@ define( require => {
       return 0.5 * this.mass * this.velocity.magnitudeSquared();
     }
 
+    get kineticEnergy() { return this.getKineticEnergy(); }
+
     /**
      * Determines the new state of the particle using the Verlet method
      *
@@ -90,66 +76,17 @@ define( require => {
      */
     step( dt ) {
 
-      //TODO JAVA java.CollidableBody
-      {
-        // Save the location and velocity before they are updated. This information is used in collision calculations.
-        this.previousLocation = this.location.copy();
-        this.previousVelocity = this.velocity.copy();
-      }
+      this.location = new Vector2(
+        this.location.x + dt * this.velocity.x + dt * dt * this.acceleration.x / 2,
+        this.location.y + dt * this.velocity.y + dt * dt * this.acceleration.y / 2
+      );
 
-      //TODO JAVA java.Particle
-      {
-        this.location = new Vector2(
-          this.location.x + dt * this.velocity.x + dt * dt * this.acceleration.x / 2,
-          this.location.y + dt * this.velocity.y + dt * dt * this.acceleration.y / 2
-        );
+      this.velocity = new Vector2(
+        this.velocity.x + dt * this.acceleration.x,
+        this.velocity.y + dt * this.acceleration.y
+      );
 
-        this.velocity = new Vector2(
-          this.velocity.x + dt * ( this.acceleration.x + this.previousAcceleration.x ) / 2,
-          this.velocity.y + dt * ( this.acceleration.y + this.previousAcceleration.y ) / 2
-        );
-
-        // Acceleration is due to gravity, and will be changed if gravity is changed.
-
-        this.previousAcceleration = this.acceleration.copy();
-      }
-
-      //TODO JAVA java.Body
-      {
-        this.momentum = this.velocity.times( this.mass );
-      }
-    }
-
-    addContainedBody( body ) {
-      assert && assert( !this.containsBody( body ), 'already contains body' );
-      this.containedBodies.push( body );
-    }
-
-    removeContainedBody( body ) {
-      assert && assert( this.containsBody( body ), 'does not contain body' );
-      this.containedBodies.splice( this.containedBodies.indexOf( body ), 1 );
-    }
-
-    containsBody( body ) {
-      return ( this.containedBodies.indexOf( body ) !== -1 );
-    }
-
-    getNumberOfContainedBodies() {
-      return this.containedBodies.length;
-    }
-
-    addConstraint( constraint ) {
-      assert && assert( !this.containsConstraint( constraint ), 'already contains constraint' );
-      this.constraints.push( constraint );
-    }
-
-    removeConstraint( constraint ) {
-      assert && assert( this.containsConstraint( constraint ), 'does not contain constraint' );
-      this.constraints.splice( this.constraints.indexOf( constraint ), 1 );
-    }
-
-    containsConstraint( constraint ) {
-      return ( this.constraints.indexOf( constraint ) !== -1 );
+      this.momentum = this.velocity.times( this.mass );
     }
 
     /**
