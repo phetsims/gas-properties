64709c9e16031764d712d2da73fa97f0aaeb18b6
Chris Malley <cmalley@pixelzoom.com>
2019-02-18 15:28:09 -0700

    doc

--- js/common/view/ParticleNode.js
+++ js/common/view/ParticleNode.js
@@ -30,7 +30,7 @@ define( require => {
     }
 
     /**
-     * Create an HTMLCanvasElement for a specified Particle, to be drawn with CanvasNode.
+     * Creates an HTMLCanvasElement for a specified Particle, to be drawn with CanvasNode.
      * @param {Particle} particle
      * @param {function} callback, see Node.toCanvas for signature
      * @returns {HTMLCanvasElement}
