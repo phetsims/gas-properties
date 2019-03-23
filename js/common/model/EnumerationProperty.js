8fe511b58b071c7d249d140b0908c3ec4cb94756
Chris Malley <cmalley@pixelzoom.com>
2019-03-22 18:44:43 -0600

    create EnumerationProperty, https://github.com/phetsims/axon/issues/239

---
+++ js/common/model/EnumerationProperty.js
@@ -0,0 +1,45 @@
+// Copyright 2019, University of Colorado Boulder
+
+/**
+ * Property whose value is a member of an Enumeration.
+ *
+ * @author Chris Malley (PixelZoom, Inc.)
+ */
+define( require => {
+  'use strict';
+
+  // modules
+  const gasProperties = require( 'GAS_PROPERTIES/gasProperties' );
+  const EnumerationIO = require( 'PHET_CORE/EnumerationIO' );
+  const Property = require( 'AXON/Property' );
+  const PropertyIO = require( 'AXON/PropertyIO' );
+
+  class EnumerationProperty extends Property {
+
+    constructor( enumeration, initialValue, options ) {
+
+      if ( options ) {
+
+        // client cannot specify superclass options that are not supported by EnumerationProperty
+        assert && assert( options.hasOwnProperty( 'isValidValue' ), 'EnumerationProperty does not support isValidValue' );
+        assert && assert( options.hasOwnProperty( 'valueType' ), 'EnumerationProperty does not support valueType' );
+
+        // client cannot specify superclass options that are controlled by EnumerationProperty
+        assert && assert( options.hasOwnProperty( 'validValues' ), 'EnumerationProperty sets validValues' );
+        assert && assert( options.hasOwnProperty( 'phetioType' ), 'EnumerationProperty sets phetioType' );
+      }
+
+      options = _.extend( {
+        validValue: enumeration.VALUES,
+        phetioType: PropertyIO( EnumerationIO( enumeration ) ),
+      }, options );
+
+      super( initialValue, options);
+
+      // @public (read-only)
+      this.enumeration = enumeration;
+    }
+  }
+
+  return gasProperties.register( 'EnumerationProperty', EnumerationProperty );
+} );
