7ab5bbfc59f3656ee41ed2d48c4847595f7a4062
Chris Malley <cmalley@pixelzoom.com>
2019-05-14 15:26:57 -0600

    delete TODO
    Signed-off-by: Chris Malley <cmalley@pixelzoom.com>

--- js/common/view/LidNode.js
+++ js/common/view/LidNode.js
@@ -1,6 +1,5 @@
 // Copyright 2018-2019, University of Colorado Boulder
 
-//TODO pass in GasPropertiesContainer.lidWidthProperty instead of baseWidth
 /**
  * The lid on the top of the container. The lid is composed of 2 pieces, a handle and a base.
  * Origin is at bottom-left of base.
