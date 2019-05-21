6bbf67fbeaf15a45b5d1d3a9009161dd1d2877a3
Chris Malley <cmalley@pixelzoom.com>
2019-05-21 12:49:15 -0600

    doc

--- js/common/GasPropertiesQueryParameters.js
+++ js/common/GasPropertiesQueryParameters.js
@@ -23,15 +23,15 @@ define( require => {
     // For internal use only, not public facing.
     canvasBounds: { type: 'flag' },
 
-    // Shows how the collision detection space is partitioned into a 2D grid of regions.
+    // Shows how the collision detection space is partitioned into a 2D grid of regions, see RegionsNode.
     // For internal use only, not public facing.
     regions: { type: 'flag' },
 
-    // Shows a 2D grid for the model coordinate frame.
+    // Shows a 2D grid for the model coordinate frame, see ModelGridNode.
     // For internal use only, not public facing.
     grid: { type: 'flag' },
 
-    // Shows the model and view coordinates that correspond to the cursor location.
+    // Shows the model and view coordinates that correspond to the cursor location, see PointerCoordinatesNode.
     // For internal use only, not public facing.
     pointerCoordinates: { type: 'flag' },
 
