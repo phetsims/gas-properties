48cba74bc5b4ec416ce28dd0356ec10c92a23634
Chris Malley <cmalley@pixelzoom.com>
2019-05-22 21:24:02 -0600

    remove units

--- js/common/view/PointerCoordinatesNode.js
+++ js/common/view/PointerCoordinatesNode.js
@@ -69,7 +69,7 @@ define( require => {
           const yModel = Util.toFixed( modelPoint.y, options.modelDecimalPlaces );
 
           // Update coordinates display.
-          textNode.text = `(${xView},${yView})<br>(${xModel},${yModel}) pm`;
+          textNode.text = `(${xView},${yView})<br>(${xModel},${yModel})`;
 
           // Resize background
           backgroundNode.setRect( 0, 0, textNode.width + 4, textNode.height + 4 );
