phetsims/gas-properties

commit 1fa66c410b8276a80806f9d868060fa45b48b160

move iterator into assignParticlesToRegions and doParticleContainerCollisions

js/common/model/CollisionDetector.js
@@ -101,12 +101,10 @@ define( require => {
 
       // Use regions that intersect the container, since collisions only occur inside the container.
       const containerRegions = _.filter( this.regions,
-          region => this.container.bounds.intersectsBounds( region.bounds ) );
+        region => this.container.bounds.intersectsBounds( region.bounds ) );
 
       // put particles in regions
-      for ( let i = 0; i < this.particleArrays.length; i++ ) {
-        assignParticlesToRegions( this.particleArrays[ i ], containerRegions );
-      }
+      assignParticlesToRegions( this.particleArrays, containerRegions );
 
       // particle-particle collisions, within each region
       if ( this.particleParticleCollisionsEnabledProperty.value ) {
@@ -116,26 +114,12 @@ define( require => {
       }
 
       // particle-container collisions
-      this.numberOfParticleContainerCollisions = this.stepParticleContainerCollisions( dt );
+      this.numberOfParticleContainerCollisions =
+        doParticleContainerCollisions( this.particleArrays, this.container.bounds, this.container.leftWallVelocity );
 
       // Verify that particles are fully inside the container.
       assert && assert( this.container.containsParticles( this.particleArrays ),
         'particles have leaked out of the container' );
-    }
-
-    /**
-     * Detects and handles particle-container collisions for the system for one time step.
-     * @param {number} dt
-     * @returns {number} the number of collisions
-     * @protected
-     */
-    stepParticleContainerCollisions( dt ) {
-      let numberOfParticleContainerCollisions = 0;
-      for ( let i = 0; i < this.particleArrays.length; i++ ) {
-        numberOfParticleContainerCollisions +=
-          doParticleContainerCollisions( this.particleArrays[ i ], this.container.bounds, this.container.leftWallVelocity );
-      }
-      return numberOfParticleContainerCollisions;
     }
   }
 
@@ -168,14 +152,19 @@ define( require => {
 
   /**
    * Assigns each particle to the Regions that it intersects, accounting for particle radius.
-   * @param {Particle[]} particles
+   * @param {Particle[]} particleArrays
    * @param {Region[]} regions
    */
-  function assignParticlesToRegions( particles, regions ) {
-    for ( let i = 0; i < particles.length; i++ ) {
-      for ( let j = 0; j < regions.length; j++ ) {
-        if ( particles[ i ].intersectsBounds( regions[ j ].bounds ) ) {
-          regions[ j ].addParticle( particles[ i ] );
+  function assignParticlesToRegions( particleArrays, regions ) {
+    for ( let i = 0; i < particleArrays.length; i++ ) {
+      const particles = particleArrays[ i ];
+      for ( let j = 0; j < particles.length; j++ ) {
+        const particle = particles[ j ];
+        for ( let k = 0; k < regions.length; k++ ) {
+          const region = regions[ k ];
+          if ( particle.intersectsBounds( region.bounds ) ) {
+            region.addParticle( particle );
+          }
         }
       }
     }
@@ -289,49 +278,56 @@ define( require => {
 
   /**
    * Detects and handles particle-container collisions.
-   * @param {Particle[]} particles
+   * @param {Particle[][]} particleArrays
    * @param {Bounds2} containerBounds
    * @param {Vector2} leftWallVelocity - velocity of the container's left (movable) wall
    * @returns {number} number of collisions
    */
-  function doParticleContainerCollisions( particles, containerBounds, leftWallVelocity ) {
+  function doParticleContainerCollisions( particleArrays, containerBounds, leftWallVelocity ) {
 
     let numberOfCollisions = 0;
-    for ( let i = 0; i < particles.length; i++ ) {
 
-      const particle = particles[ i ];
-      let collided = false;
+    for ( let i = 0; i < particleArrays.length; i++ ) {
 
-      // adjust x
-      if ( particle.left <= containerBounds.minX ) {
-        particle.left = containerBounds.minX;
+      const particles = particleArrays[ i ];
 
-        // If the left wall is moving, it will do work.
-        particle.setVelocityXY( -( particle.velocity.x - leftWallVelocity.x ), particle.velocity.y );
-        collided = true;
-      }
-      else if ( particle.right >= containerBounds.maxX ) {
-        particle.right = containerBounds.maxX;
-        particle.setVelocityXY( -particle.velocity.x, particle.velocity.y );
-        collided = true;
-      }
+      for ( let j = 0; j < particles.length; j++ ) {
 
-      // adjust y
-      if ( particle.top >= containerBounds.maxY ) {
-        particle.top = containerBounds.maxY;
-        particle.setVelocityXY( particle.velocity.x, -particle.velocity.y );
-        collided = true;
-      }
-      else if ( particle.bottom <= containerBounds.minY ) {
-        particle.bottom = containerBounds.minY;
-        particle.setVelocityXY( particle.velocity.x, -particle.velocity.y );
-        collided = true;
-      }
+        const particle = particles[ j ];
+        let collided = false;
 
-      if ( collided ) {
-        numberOfCollisions++;
+        // adjust x
+        if ( particle.left <= containerBounds.minX ) {
+          particle.left = containerBounds.minX;
+
+          // If the left wall is moving, it will do work.
+          particle.setVelocityXY( -( particle.velocity.x - leftWallVelocity.x ), particle.velocity.y );
+          collided = true;
+        }
+        else if ( particle.right >= containerBounds.maxX ) {
+          particle.right = containerBounds.maxX;
+          particle.setVelocityXY( -particle.velocity.x, particle.velocity.y );
+          collided = true;
+        }
+
+        // adjust y
+        if ( particle.top >= containerBounds.maxY ) {
+          particle.top = containerBounds.maxY;
+          particle.setVelocityXY( particle.velocity.x, -particle.velocity.y );
+          collided = true;
+        }
+        else if ( particle.bottom <= containerBounds.minY ) {
+          particle.bottom = containerBounds.minY;
+          particle.setVelocityXY( particle.velocity.x, -particle.velocity.y );
+          collided = true;
+        }
+
+        if ( collided ) {
+          numberOfCollisions++;
+        }
       }
     }
+
     return numberOfCollisions;
   }
 

commit 3231519106fbf26a1d29a75d35e522568ba517a5

fix particle-container collisions that I broke in https://github.com/phetsims/gas-properties/commit/1fa66c410b8276a80806f9d868060fa45b48b160

js/common/model/CollisionDetector.js
@@ -112,12 +112,25 @@ define( require => {
       }
 
       // particle-container collisions
-      this.numberOfParticleContainerCollisions =
-        doParticleContainerCollisions( this.particleArrays, this.container.bounds, this.container.leftWallVelocity );
+      this.numberOfParticleContainerCollisions = this.updateParticleContainerCollisions();
 
       // Verify that particles are fully inside the container.
       assert && assert( this.container.containsParticles( this.particleArrays ),
         'particles have leaked out of the container' );
+    }
+
+    /**
+     * Detects and handles particle-container collisions for the system for one time step.
+     * @returns {number} the number of collisions
+     * @protected
+     */
+    updateParticleContainerCollisions() {
+      let numberOfParticleContainerCollisions = 0;
+      for ( let i = 0; i < this.particleArrays.length; i++ ) {
+        numberOfParticleContainerCollisions +=
+          doParticleContainerCollisions( this.particleArrays[ i ], this.container.bounds, this.container.leftWallVelocity );
+      }
+      return numberOfParticleContainerCollisions;
     }
   }
 
@@ -222,7 +235,7 @@ define( require => {
           adjustParticleLocation( particle1, contactPointX, contactPointY, lineAngle,
             mutableVectors.pointOnLine, mutableVectors.reflectedPoint );
           adjustParticleLocation( particle2, contactPointX, contactPointY, lineAngle,
-                      mutableVectors.pointOnLine, mutableVectors.reflectedPoint );
+            mutableVectors.pointOnLine, mutableVectors.reflectedPoint );
 
           //-----------------------------------------------------------------------------------------
           // Adjust particle velocities using impulse-based contact model.
@@ -294,56 +307,51 @@ define( require => {
 
   /**
    * Detects and handles particle-container collisions.
-   * @param {Particle[][]} particleArrays
+   * @param {Particle[]} particles
    * @param {Bounds2} containerBounds
    * @param {Vector2} leftWallVelocity - velocity of the container's left (movable) wall
    * @returns {number} number of collisions
    */
-  function doParticleContainerCollisions( particleArrays, containerBounds, leftWallVelocity ) {
-    assert && assert( Array.isArray( particleArrays ), `invalid particleArrays: ${particleArrays}` );
+  function doParticleContainerCollisions( particles, containerBounds, leftWallVelocity ) {
+    assert && assert( Array.isArray( particles ), `invalid particles: ${particles}` );
     assert && assert( containerBounds instanceof Bounds2, `invalid containerBounds: ${containerBounds}` );
     assert && assert( leftWallVelocity instanceof Vector2, `invalid leftWallVelocity: ${leftWallVelocity}` );
 
     let numberOfCollisions = 0;
 
-    for ( let i = 0; i < particleArrays.length; i++ ) {
+    for ( let i = 0; i < particles.length; i++ ) {
 
-      const particles = particleArrays[ i ];
+      const particle = particles[ i ];
+      let collided = false;
 
-      for ( let j = 0; j < particles.length; j++ ) {
+      // adjust x
+      if ( particle.left <= containerBounds.minX ) {
+        particle.left = containerBounds.minX;
 
-        const particle = particles[ j ];
-        let collided = false;
+        // If the left wall is moving, it will do work.
+        particle.setVelocityXY( -( particle.velocity.x - leftWallVelocity.x ), particle.velocity.y );
+        collided = true;
+      }
+      else if ( particle.right >= containerBounds.maxX ) {
+        particle.right = containerBounds.maxX;
+        particle.setVelocityXY( -particle.velocity.x, particle.velocity.y );
+        collided = true;
+      }
 
-        // adjust x
-        if ( particle.left <= containerBounds.minX ) {
-          particle.left = containerBounds.minX;
+      // adjust y
+      if ( particle.top >= containerBounds.maxY ) {
+        particle.top = containerBounds.maxY;
+        particle.setVelocityXY( particle.velocity.x, -particle.velocity.y );
+        collided = true;
+      }
+      else if ( particle.bottom <= containerBounds.minY ) {
+        particle.bottom = containerBounds.minY;
+        particle.setVelocityXY( particle.velocity.x, -particle.velocity.y );
+        collided = true;
+      }
 
-          // If the left wall is moving, it will do work.
-          particle.setVelocityXY( -( particle.velocity.x - leftWallVelocity.x ), particle.velocity.y );
-          collided = true;
-        }
-        else if ( particle.right >= containerBounds.maxX ) {
-          particle.right = containerBounds.maxX;
-          particle.setVelocityXY( -particle.velocity.x, particle.velocity.y );
-          collided = true;
-        }
-
-        // adjust y
-        if ( particle.top >= containerBounds.maxY ) {
-          particle.top = containerBounds.maxY;
-          particle.setVelocityXY( particle.velocity.x, -particle.velocity.y );
-          collided = true;
-        }
-        else if ( particle.bottom <= containerBounds.minY ) {
-          particle.bottom = containerBounds.minY;
-          particle.setVelocityXY( particle.velocity.x, -particle.velocity.y );
-          collided = true;
-        }
-
-        if ( collided ) {
-          numberOfCollisions++;
-        }
+      if ( collided ) {
+        numberOfCollisions++;
       }
     }
 

js/diffusion/model/DiffusionCollisionDetector.js
@@ -34,13 +34,11 @@ define( require => {
 
     /**
      * Detects and handles particle-container collisions for the system for one time step.
-     * @param {number} dt
      * @returns {number} the number of collisions
      * @protected
      * @override
      */
-    stepParticleContainerCollisions( dt ) {
-      assert && assert( typeof dt === 'number' && dt > 0, `invalid dt: ${dt}` );
+    updateParticleContainerCollisions() {
 
       let numberOfParticleContainerCollisions = 0;
       if ( this.container.hasDividerProperty.value ) {
@@ -55,7 +53,7 @@ define( require => {
       else {
 
         // If there is no divider, use default behavior.
-        numberOfParticleContainerCollisions = super.stepParticleContainerCollisions( dt );
+        numberOfParticleContainerCollisions = super.updateParticleContainerCollisions();
       }
       return numberOfParticleContainerCollisions;
     }