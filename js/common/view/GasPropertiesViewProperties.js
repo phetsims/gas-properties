c4d6ceb8268045e6b623439e8871416a8c35fd75
Chris Malley <cmalley@pixelzoom.com>
2019-05-26 19:57:31 -0600

    doc

--- js/common/view/GasPropertiesViewProperties.js
+++ js/common/view/GasPropertiesViewProperties.js
@@ -1,7 +1,7 @@
 // Copyright 2018-2019, University of Colorado Boulder
 
 /**
- * Base class for view-specific Properties in the Intro, Explore, and Energy screens.
+ * Base class for view-specific Properties that are common to the Intro, Explore, and Energy screens.
  *
  * @author Chris Malley (PixelZoom, Inc.)
  */
