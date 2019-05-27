593376df1b39a48a3ef11176e052a3acc37426ef
Chris Malley <cmalley@pixelzoom.com>
2019-05-26 23:43:28 -0600

    factor out boilerplate in OopsDialogManager

--- js/common/view/OopsDialogManager.js
+++ js/common/view/OopsDialogManager.js
@@ -5,7 +5,7 @@
  * When holding a quantity constant would break the model, the model puts itself in a sane configurations,
  * the model notifies the view via an Emitter, and the view notifies the user via a dialog.
  * Dialogs are created on demand, then cached for reuse.
- * 
+ *
  * @author Chris Malley (PixelZoom, Inc.)
  */
 define( require => {
@@ -14,14 +14,14 @@ define( require => {
   // modules
   const gasProperties = require( 'GAS_PROPERTIES/gasProperties' );
   const GasPropertiesConstants = require( 'GAS_PROPERTIES/common/GasPropertiesConstants' );
-  const OopsDialog = require( 'SCENERY_PHET/OopsDialog' );  
+  const OopsDialog = require( 'SCENERY_PHET/OopsDialog' );
 
   // string
   const oopsTemperatureWithEmptyContainerString = require( 'string!GAS_PROPERTIES/oopsTemperatureWithEmptyContainer' );
   const oopsPressureWithEmptyContainerString = require( 'string!GAS_PROPERTIES/oopsPressureWithEmptyContainer' );
   const oopsPressureWithLargeVolumeString = require( 'string!GAS_PROPERTIES/oopsPressureWithLargeVolume' );
   const oopsPressureWithSmallVolumeString = require( 'string!GAS_PROPERTIES/oopsPressureWithSmallVolume' );
-  
+
   class OopsDialogManager {
 
     /**
@@ -29,37 +29,45 @@ define( require => {
      */
     constructor( oops ) {
 
+      // @private dialogs are cached in a map, with {string} key and {OopsDialog} value
+      this.dialogCache = {};
+
       // Oops! Temperature cannot be held constant when the container is empty.
-      let temperatureWithEmptyContainerDialog = null;
       oops.temperatureWithEmptyContainerEmitter.addListener( () => {
-        temperatureWithEmptyContainerDialog = temperatureWithEmptyContainerDialog || 
-                                              new OopsDialog( oopsTemperatureWithEmptyContainerString, GasPropertiesConstants.OOPS_DIALOG_OPTIONS );
-        temperatureWithEmptyContainerDialog.show();
+        this.showDialog( oopsTemperatureWithEmptyContainerString );
       } );
 
       // Oops! Pressure cannot be held constant when the container is empty.
-      let pressureWithEmptyContainerDialog = null;
       oops.pressureWithEmptyContainerEmitter.addListener( () => {
-        pressureWithEmptyContainerDialog = pressureWithEmptyContainerDialog || 
-                                           new OopsDialog( oopsPressureWithEmptyContainerString, GasPropertiesConstants.OOPS_DIALOG_OPTIONS );
-        pressureWithEmptyContainerDialog.show();
+        this.showDialog( oopsPressureWithEmptyContainerString );
       } );
 
       // Oops! Pressure cannot be held constant. Volume would be too large.
-      let pressureWithLargeVolumeDialog = null;
       oops.pressureWithLargeVolumeEmitter.addListener( () => {
-        pressureWithLargeVolumeDialog = pressureWithLargeVolumeDialog || 
-                                        new OopsDialog( oopsPressureWithLargeVolumeString, GasPropertiesConstants.OOPS_DIALOG_OPTIONS );
-        pressureWithLargeVolumeDialog.show();
+        this.showDialog( oopsPressureWithLargeVolumeString );
       } );
 
       // Oops! Pressure cannot be held constant. Volume would be too small.
-      let pressureWithSmallVolumeDialog = null;
       oops.pressureWithSmallVolumeEmitter.addListener( () => {
-        pressureWithSmallVolumeDialog = pressureWithSmallVolumeDialog || 
-                                        new OopsDialog( oopsPressureWithSmallVolumeString, GasPropertiesConstants.OOPS_DIALOG_OPTIONS );
-        pressureWithSmallVolumeDialog.show();
+        this.showDialog( oopsPressureWithSmallVolumeString );
       } );
+    }
+
+    /**
+     * Shows a dialog for the specified message.
+     * If a dialog has not previously been shown for the message, a dialog is created and added to the cache.
+     * @param {string} message
+     * @private
+     */
+    showDialog( message ) {
+      assert && assert( typeof message === 'string', `invalid message: ${message}` );
+
+      let dialog = this.dialogCache[ message ];
+      if ( !dialog ) {
+        dialog = new OopsDialog( message, GasPropertiesConstants.OOPS_DIALOG_OPTIONS );
+        this.dialogCache[ message ] = dialog;
+      }
+      dialog.show();
     }
   }
 
