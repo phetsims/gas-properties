5cb59954bab3e254c4c56a8e6fc23fef0f37e0c8
zepumph <michael.kauzmann@colorado.edu>
2019-10-03 15:12:39 -0800

    ObjectIO.supertype === null, factor out getPrototypeOf usages to ObjectIO.getSupertype, https://github.com/phetsims/phet-io-wrappers/issues/298

--- js/phet-io/gas-properties-phet-io-types.js
+++ js/phet-io/gas-properties-phet-io-types.js
@@ -823,7 +823,7 @@ window.phet.phetio.phetioTypes = assert &&
       "events": [],
       "methodOrder": [],
       "methods": {},
-      "supertype": "Object",
+      "supertype": null,
       "typeName": "ObjectIO"
     },
     "PhetButtonIO": {
