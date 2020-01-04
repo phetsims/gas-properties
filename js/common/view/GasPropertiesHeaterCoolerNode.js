279829178da8473978fac93c1d14e01b4b6678c9
phet-dev <phetadmin@gmail.com>
2020-01-04 03:14:17 -0700

    update copyright dates from daily grunt work

--- js/common/view/GasPropertiesHeaterCoolerNode.js
+++ js/common/view/GasPropertiesHeaterCoolerNode.js
@@ -1,4 +1,4 @@
-// Copyright 2018-2019, University of Colorado Boulder
+// Copyright 2018-2020, University of Colorado Boulder
 
 /**
  * GasPropertiesHeaterCoolerNode is a specialization of HeaterCoolerNode for this sim.  Responsibilities include:
--- js/common/view/IdealGasLawScreenView.js
+++ js/common/view/IdealGasLawScreenView.js
@@ -1,4 +1,4 @@
-// Copyright 2018-2019, University of Colorado Boulder
+// Copyright 2018-2020, University of Colorado Boulder
 
 /**
  * IdealGasLawScreenView is the ScreenView base class for screens that are based on the Ideal Gas Law.
