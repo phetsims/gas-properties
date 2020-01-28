5a3d610b5055794e905809eea91dbbd89d7df614
zepumph <michael.kauzmann@colorado.edu>
2020-01-27 16:58:14 -0900

    add phetioDocumentation, https://github.com/phetsims/joist/issues/591

--- js/phet-io/gas-properties-phet-io-elements-baseline.js
+++ js/phet-io/gas-properties-phet-io-elements-baseline.js
@@ -11130,7 +11130,7 @@ window.phet.phetio.phetioElementsBaseline = assert &&
       "phetioTypeName": "PropertyIO<BooleanIO>"
     },
     "gasProperties.general.soundManager.enabledProperty": {
-      "phetioDocumentation": "",
+      "phetioDocumentation": "If this sim contains sound, then this Property will toggle whether sound is enabled ordisabled.",
       "phetioDynamicElement": false,
       "phetioEventType": "MODEL",
       "phetioFeatured": false,
