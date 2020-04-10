4a76dcff69f7538d1fdad6f814714eaf2b88c547
chrisklus <christopher.klusendorf@colorado.edu>
2020-04-10 16:53:05 -0400

    Update baseline files, see https://github.com/phetsims/joist/issues/623

--- js/phet-io/gas-properties-baseline.js
+++ js/phet-io/gas-properties-baseline.js
@@ -18358,7 +18358,7 @@ window.phet.preloads.phetio.phetioElementsBaseline = assert &&
       "phetioTypeName": "ActionIO<NumberIO, NumberIO>"
     },
     "gasProperties.general.model.screenProperty": {
-      "phetioDocumentation": "Which sim screen is selected, including the home screen",
+      "phetioDocumentation": "Determines which screen is selected in the simulation",
       "phetioDynamicElement": false,
       "phetioEventType": "MODEL",
       "phetioFeatured": true,
