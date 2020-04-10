919eb4ae8a9daaea50862deed8443593c6f4ebaf
pixelzoom <cmalley@pixelzoom.com>
2020-04-10 13:46:02 -0600

    revise IO Type doc for primitive core types, https://github.com/phetsims/tandem/issues/165

--- js/phet-io/gas-properties-types.js
+++ js/phet-io/gas-properties-types.js
@@ -242,7 +242,7 @@ window.phet.preloads.phetio.phetioTypes = assert &&
       "typeName": "ArrayIO<StringIO>"
     },
     "BooleanIO": {
-      "documentation": "Wrapper for the built-in JS boolean type (true/false)",
+      "documentation": "IO Type for Javascript's boolean primitive type",
       "events": [],
       "methodOrder": [],
       "methods": {},
@@ -1008,7 +1008,7 @@ window.phet.preloads.phetio.phetioTypes = assert &&
       "typeName": "NumberDisplayIO"
     },
     "NumberIO": {
-      "documentation": "Wrapper for the built-in JS number type (floating point, but also represents integers)",
+      "documentation": "IO Type for Javascript's number primitive type",
       "events": [],
       "methodOrder": [],
       "methods": {},
@@ -2095,7 +2095,7 @@ window.phet.preloads.phetio.phetioTypes = assert &&
       "typeName": "SliderIO"
     },
     "StringIO": {
-      "documentation": "Wrapper for the built-in JS string type",
+      "documentation": "IO Type for Javascript's string primitive type",
       "events": [],
       "methodOrder": [],
       "methods": {},
