191584e7b64620f8798b3065ccfafa5a4e70591b
chrisklus <christopher.klusendorf@colorado.edu>
2020-04-27 17:02:02 -0400

    Update phet-io files, see https://github.com/phetsims/joist/issues/628

--- js/phet-io/gas-properties-baseline.js
+++ js/phet-io/gas-properties-baseline.js
@@ -20964,7 +20964,7 @@ window.phet.preloads.phetio.phetioElementsBaseline = assert &&
       "phetioStudioControl": true,
       "phetioTypeName": "PropertyIO<BooleanIO>"
     },
-    "gasProperties.general.view.simNameProperty": {
+    "gasProperties.general.model.simNameProperty": {
       "phetioDocumentation": "The name of the sim. Changing this value will update the title text on the navigation bar and the title text on the home screen, if it exists.",
       "phetioDynamicElement": false,
       "phetioEventType": "MODEL",
