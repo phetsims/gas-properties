09cd3207906fc8ccabb17bec30242374430fe7a1
phet-dev <phetadmin@gmail.com>
2021-05-25 15:48:54 -0600

    update copyright dates from daily grunt work

--- js/common/GasPropertiesQueryParameters.js
+++ js/common/GasPropertiesQueryParameters.js
@@ -1,4 +1,4 @@
-// Copyright 2018-2020, University of Colorado Boulder
+// Copyright 2018-2021, University of Colorado Boulder
 
 /**
  * GasPropertiesQueryParameters defines the query parameters that are specific to this sim.
