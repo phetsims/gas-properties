5ab28bc4725443da6c42c888cb28eff738450186
samreid <reids@colorado.edu>
2021-08-20 16:15:55 -0600

    Add Namespace parameter to ProfileColorProperty constructor, see https://github.com/phetsims/scenery/issues/1259

--- js/common/GasPropertiesColors.js
+++ js/common/GasPropertiesColors.js
@@ -16,109 +16,109 @@ const GasPropertiesColors = {
   // These colors change based on which profile is selected.
   //------------------------------------------------------------------------------------------------------------------
 
-  screenBackgroundColorProperty: new ProfileColorProperty( 'screenBackgroundColor', {
+  screenBackgroundColorProperty: new ProfileColorProperty( gasProperties, 'screenBackgroundColor', {
     default: 'black',
     projector: 'white'
   } ),
 
   // fill for control panels
-  panelFillProperty: new ProfileColorProperty( 'panelFill', {
+  panelFillProperty: new ProfileColorProperty( gasProperties, 'panelFill', {
     default: 'rgb( 40, 40, 40 )',
     projector: 'rgb( 235, 235, 235 )'
   } ),
 
   // stroke for control panels
-  panelStrokeProperty: new ProfileColorProperty( 'panelStroke', {
+  panelStrokeProperty: new ProfileColorProperty( gasProperties, 'panelStroke', {
     default: 'rgb( 55, 55, 55 )',
     projector: 'rgb( 220, 220, 220 )'
   } ),
 
   // default fill for text
-  textFillProperty: new ProfileColorProperty( 'textFill', {
+  textFillProperty: new ProfileColorProperty( gasProperties, 'textFill', {
     default: 'white',
     projector: 'black'
   } ),
 
   // fill for check boxes
-  checkboxFillProperty: new ProfileColorProperty( 'checkboxFill', {
+  checkboxFillProperty: new ProfileColorProperty( gasProperties, 'checkboxFill', {
     default: 'black',
     projector: 'white'
   } ),
 
   // stroke for check boxes
-  checkboxStrokeProperty: new ProfileColorProperty( 'checkboxStroke', {
+  checkboxStrokeProperty: new ProfileColorProperty( gasProperties, 'checkboxStroke', {
     default: 'white',
     projector: 'black'
   } ),
 
   // radio buttons for choosing particle type
-  radioButtonGroupBaseColorProperty: new ProfileColorProperty( 'radioButtonGroupBaseColor', {
+  radioButtonGroupBaseColorProperty: new ProfileColorProperty( gasProperties, 'radioButtonGroupBaseColor', {
     default: 'black',
     projector: 'white'
   } ),
 
   // radio buttons for choosing particle type
-  radioButtonGroupDeselectedStrokeProperty: new ProfileColorProperty( 'radioButtonGroupDeselectedStroke', {
+  radioButtonGroupDeselectedStrokeProperty: new ProfileColorProperty( gasProperties, 'radioButtonGroupDeselectedStroke', {
     default: 'rgb( 240, 240, 240 )',
     projector: 'rgb( 180, 180, 180 )'
   } ),
 
   // walls of the container
-  containerBoundsStrokeProperty: new ProfileColorProperty( 'containerBoundsStroke', {
+  containerBoundsStrokeProperty: new ProfileColorProperty( gasProperties, 'containerBoundsStroke', {
     default: 'white',
     projector: 'black'
   } ),
 
   // bounds of the previous container size, shown while the container is being resized
-  containerPreviousBoundsStrokeProperty: new ProfileColorProperty( 'containerPreviousBoundsStroke', {
+  containerPreviousBoundsStrokeProperty: new ProfileColorProperty( gasProperties, 'containerPreviousBoundsStroke', {
     default: 'rgb( 100, 100, 100 )',
     projector: 'rgb( 220, 220, 220 )'
   } ),
 
   // base of the lid, the part that the handle attaches to
-  lidBaseFillProperty: new ProfileColorProperty( 'lidBaseFill', {
+  lidBaseFillProperty: new ProfileColorProperty( gasProperties, 'lidBaseFill', {
     default: 'rgb( 180, 180, 180 )',
     projector: 'rgb( 128, 128, 128 )'
   } ),
 
   // dimensional arrow that appears below the container
-  sizeArrowColorProperty: new ProfileColorProperty( 'sizeArrowColor', {
+  sizeArrowColorProperty: new ProfileColorProperty( gasProperties, 'sizeArrowColor', {
     default: 'white',
     projector: 'black'
   } ),
 
   // icon for the 'Width' checkbox
-  widthIconColorProperty: new ProfileColorProperty( 'widthIconColor', {
+  widthIconColorProperty: new ProfileColorProperty( gasProperties, 'widthIconColor', {
     default: 'white',
     projector: 'black'
   } ),
 
   // stroke around center-of-mass indicators
-  centerOfMassStrokeProperty: new ProfileColorProperty( 'centerOfMassStroke', {
+  centerOfMassStrokeProperty: new ProfileColorProperty( gasProperties, 'centerOfMassStroke', {
     default: 'white',
     projector: 'black'
   } ),
 
   // the scale that appears below the container in the Diffusion screen
-  scaleColorProperty: new ProfileColorProperty( 'scaleColor', {
+  scaleColorProperty: new ProfileColorProperty( gasProperties, 'scaleColor', {
     default: 'rgb( 220, 220, 220 )',
     projector: 'rgb( 100, 100, 100 )'
   } ),
 
   // enabled with ?grid query parameter
-  gridColorProperty: new ProfileColorProperty( 'gridColor', {
+  gridColorProperty: new ProfileColorProperty( gasProperties, 'gridColor', {
     default: 'white',
     projector: 'black'
   } ),
 
   // enabled with ?pointerCoordinates query parameter
-  pointerCoordinatesTextColorProperty: new ProfileColorProperty( 'pointerCoordinatesTextColor', {
+  pointerCoordinatesTextColorProperty: new ProfileColorProperty( gasProperties, 'pointerCoordinatesTextColor', {
     default: 'white',
     projector: 'black'
   } ),
 
   // enabled with ?pointerCoordinates query parameter
-  pointerCoordinatesBackgroundColorProperty: new ProfileColorProperty( 'pointerCoordinatesBackgroundColor', {
+  pointerCoordinatesBackgroundColorProperty: new ProfileColorProperty( gasProperties, 'pointerCoordinatesBackgroundColor', {
     default: 'rgba( 0, 0, 0, 0.5 )',
     projector: 'rgba( 255, 255, 255, 0.5 )'
   } ),
@@ -129,100 +129,100 @@ const GasPropertiesColors = {
   //------------------------------------------------------------------------------------------------------------------
 
   // primary color for heavy particles
-  heavyParticleColorProperty: new ProfileColorProperty( 'heavyParticleColor', {
+  heavyParticleColorProperty: new ProfileColorProperty( gasProperties, 'heavyParticleColor', {
     default: 'rgb( 119, 114, 244 )' // purple
   } ),
 
   // specular highlight for heavy particles
-  heavyParticleHighlightColorProperty: new ProfileColorProperty( 'heavyParticleHighlightColor', {
+  heavyParticleHighlightColorProperty: new ProfileColorProperty( gasProperties, 'heavyParticleHighlightColor', {
     default: 'rgb( 220, 220, 255 )' // lighter shade of heavyParticleColor
   } ),
 
   // primary color for light particles
-  lightParticleColorProperty: new ProfileColorProperty( 'lightParticleColor', {
+  lightParticleColorProperty: new ProfileColorProperty( gasProperties, 'lightParticleColor', {
     default: 'rgb( 232, 78, 32 )' // red
   } ),
 
   // specular highlight for light particles
-  lightParticleHighlightColorProperty: new ProfileColorProperty( 'lightParticleHighlightColor', {
+  lightParticleHighlightColorProperty: new ProfileColorProperty( gasProperties, 'lightParticleHighlightColor', {
     default: 'rgb( 255, 170, 170 )' // lighter shade of lightParticleColor
   } ),
 
   // primary color for 1st particle type in Diffusion screen
-  particle1ColorProperty: new ProfileColorProperty( 'particle1Color', {
+  particle1ColorProperty: new ProfileColorProperty( gasProperties, 'particle1Color', {
     default: 'rgb( 0, 230, 255)' // cyan
   } ),
 
   // specular highlight for 1st particle type in Diffusion screen
-  particle1HighlightColorProperty: new ProfileColorProperty( 'particle1HighlightColor', {
+  particle1HighlightColorProperty: new ProfileColorProperty( gasProperties, 'particle1HighlightColor', {
     default: 'rgb( 203, 247, 252 )' // lighter shade of particle1Color
   } ),
 
   // primary color for 2nd particle type in Diffusion screen
-  particle2ColorProperty: new ProfileColorProperty( 'particle2Color', {
+  particle2ColorProperty: new ProfileColorProperty( gasProperties, 'particle2Color', {
     default: 'rgb( 232, 78, 32 )' // red
   } ),
 
   // specular highlight for 2nd particle type in Diffusion screen
-  particle2HighlightColorProperty: new ProfileColorProperty( 'particle2HighlightColor', {
+  particle2HighlightColorProperty: new ProfileColorProperty( gasProperties, 'particle2HighlightColor', {
     default: 'rgb( 255, 170, 170 )' // lighter shade of particle2Color
   } ),
 
-  stopwatchBackgroundColorProperty: new ProfileColorProperty( 'stopwatchBackgroundColor', {
+  stopwatchBackgroundColorProperty: new ProfileColorProperty( gasProperties, 'stopwatchBackgroundColor', {
     default: 'rgb(  80, 130, 230  )' // blue
   } ),
 
-  collisionCounterBackgroundColorProperty: new ProfileColorProperty( 'collisionCounterBackgroundColor', {
+  collisionCounterBackgroundColorProperty: new ProfileColorProperty( gasProperties, 'collisionCounterBackgroundColor', {
     default: 'rgb( 254, 212, 131 )' // yellowish
   } ),
 
   // pseudo-3D bezel around the outer edge of the collision counter
-  collisionCounterBezelColorProperty: new ProfileColorProperty( 'collisionCounterBezelColor', {
+  collisionCounterBezelColorProperty: new ProfileColorProperty( gasProperties, 'collisionCounterBezelColor', {
     default: 'rgb( 90, 90, 90 )'
   } ),
 
   // horizontal separator in control panels
-  separatorColorProperty: new ProfileColorProperty( 'separatorColor', {
+  separatorColorProperty: new ProfileColorProperty( gasProperties, 'separatorColor', {
     default: 'rgb( 100, 100, 100)'
   } ),
 
   // radio buttons for choosing particle type
-  radioButtonGroupSelectedStrokeProperty: new ProfileColorProperty( 'radioButtonGroupSelectedStroke', {
+  radioButtonGroupSelectedStrokeProperty: new ProfileColorProperty( gasProperties, 'radioButtonGroupSelectedStroke', {
     default: 'rgb( 105, 195, 231 )' // blue
   } ),
 
   // grip on the container's lid
-  lidGripColorProperty: new ProfileColorProperty( 'lidGripColor', {
+  lidGripColorProperty: new ProfileColorProperty( gasProperties, 'lidGripColor', {
     default: 'rgb( 160, 160, 160 )'
   } ),
 
   // default grip on the container's resize handle
-  resizeGripColorProperty: new ProfileColorProperty( 'resizeGripColor', {
+  resizeGripColorProperty: new ProfileColorProperty( gasProperties, 'resizeGripColor', {
     default: 'rgb( 160, 160, 160 )'
   } ),
 
   // grip on the container's resize handle in the Ideal screen
-  idealResizeGripColorProperty: new ProfileColorProperty( 'idealResizeGripColor', {
+  idealResizeGripColorProperty: new ProfileColorProperty( gasProperties, 'idealResizeGripColor', {
     default: 'rgb( 187, 154, 86 )' // gold
   } ),
 
   // bars in the Speed histogram
-  speedHistogramBarColorProperty: new ProfileColorProperty( 'speedHistogramBarColor', {
+  speedHistogramBarColorProperty: new ProfileColorProperty( gasProperties, 'speedHistogramBarColor', {
     default: 'white'
   } ),
 
   // bars in the Kinetic Energy histogram
-  kineticEnergyHistogramBarColorProperty: new ProfileColorProperty( 'kineticEnergyHistogramBarColor', {
+  kineticEnergyHistogramBarColorProperty: new ProfileColorProperty( gasProperties, 'kineticEnergyHistogramBarColor', {
     default: PhetColorScheme.KINETIC_ENERGY
   } ),
 
   // container divider in the Diffusion screen
-  dividerColorProperty: new ProfileColorProperty( 'dividerColor', {
+  dividerColorProperty: new ProfileColorProperty( gasProperties, 'dividerColor', {
     default: 'rgb( 70, 205, 85 )'
   } ),
 
   // eraser button that is used to clear particles from the container
-  eraserButtonColorProperty: new ProfileColorProperty( 'eraserButtonColor', {
+  eraserButtonColorProperty: new ProfileColorProperty( gasProperties, 'eraserButtonColor', {
     default: 'rgb( 220, 220, 220 )'
   } )
 };
