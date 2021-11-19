641a26e5d51f39a566294c317f877504a238a239
Sam Reid <reids@colorado.edu>
2021-11-18 17:43:57 -0700

    Run grunt update, see https://github.com/phetsims/chipper/issues/1148

---
+++ js/gasPropertiesStrings.ts
@@ -0,0 +1,78 @@
+// Copyright 2020-2021, University of Colorado Boulder
+
+/**
+ * Auto-generated from modulify, DO NOT manually modify.
+ */
+/* eslint-disable */
+import getStringModule from '../../chipper/js/getStringModule.js';
+import gasProperties from './gasProperties.js';
+
+type StringsType = {
+  'gas-properties': {
+    'title': string
+  },
+  'screen': {
+    'ideal': string,
+    'explore': string,
+    'energy': string,
+    'diffusion': string
+  },
+  'holdConstant': {
+    'title': string,
+    'nothing': string,
+    'volume': string,
+    'temperature': string,
+    'pressureV': string,
+    'pressureT': string
+  },
+  'particles': string,
+  'heavy': string,
+  'light': string,
+  'width': string,
+  'stopwatch': string,
+  'collisionCounter': string,
+  'picoseconds': string,
+  'wallCollisions': string,
+  'samplePeriod': string,
+  'kelvin': string,
+  'degreesCelsius': string,
+  'valueUnits': string,
+  'pressure': string,
+  'kilopascals': string,
+  'atmospheres': string,
+  'nanometers': string,
+  'averageSpeed': string,
+  'metersPerSecond': string,
+  'speed': string,
+  'numberOfParticles': string,
+  'kineticEnergy': string,
+  'injectionTemperature': string,
+  'collisions': string,
+  'matchContainer': string,
+  'setTo': string,
+  'returnLid': string,
+  'data': string,
+  'centerOfMass': string,
+  'particleFlowRate': string,
+  'removeDivider': string,
+  'resetDivider': string,
+  'tAvg': string,
+  'tAvgK': string,
+  'massAMU': string,
+  'radiusPm': string,
+  'initialTemperatureK': string,
+  'pressureNoise': string,
+  'scale': string,
+  'oopsTemperatureEmpty': string,
+  'oopsTemperatureOpen': string,
+  'oopsPressureEmpty': string,
+  'oopsPressureLarge': string,
+  'oopsPressureSmall': string,
+  'oopsMaximumTemperature': string
+};
+
+const gasPropertiesStrings = getStringModule( 'GAS_PROPERTIES' ) as StringsType;
+
+gasProperties.register( 'gasPropertiesStrings', gasPropertiesStrings );
+
+export default gasPropertiesStrings;
