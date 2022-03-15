85ff081f079d09ddc77e5e9b0e176752d98c3f2c
phet-dev <phetadmin@gmail.com>
2022-03-15 03:26:52 -0600

    update copyright dates from daily grunt work

--- js/common/view/ParticlesNode.js
+++ js/common/view/ParticlesNode.js
@@ -1,4 +1,4 @@
-// Copyright 2019-2021, University of Colorado Boulder
+// Copyright 2019-2022, University of Colorado Boulder
 
 /**
  * ParticlesNode is the base class for rendering a collection of particles using Sprites. It is used in all screens.
