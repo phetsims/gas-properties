de94d5aba55a1c4b4ee8bc220b43ec0979741b69
Sam Reid <reids@colorado.edu>
2022-05-07 09:35:31 -0600

    Use semicolons to terminate fields in type StringType, see https://github.com/phetsims/chipper/issues/1208

--- js/gasPropertiesStrings.ts
+++ js/gasPropertiesStrings.ts
@@ -9,66 +9,66 @@ import gasProperties from './gasProperties.js';
 
 type StringsType = {
   'gas-properties': {
-    'title': string
-  },
+    'title': string;
+  };
   'screen': {
-    'ideal': string,
-    'explore': string,
-    'energy': string,
-    'diffusion': string
-  },
+    'ideal': string;
+    'explore': string;
+    'energy': string;
+    'diffusion': string;
+  };
   'holdConstant': {
-    'title': string,
-    'nothing': string,
-    'volume': string,
-    'temperature': string,
-    'pressureV': string,
-    'pressureT': string
-  },
-  'particles': string,
-  'heavy': string,
-  'light': string,
-  'width': string,
-  'stopwatch': string,
-  'collisionCounter': string,
-  'picoseconds': string,
-  'wallCollisions': string,
-  'samplePeriod': string,
-  'kelvin': string,
-  'degreesCelsius': string,
-  'valueUnits': string,
-  'pressure': string,
-  'kilopascals': string,
-  'atmospheres': string,
-  'nanometers': string,
-  'averageSpeed': string,
-  'metersPerSecond': string,
-  'speed': string,
-  'numberOfParticles': string,
-  'kineticEnergy': string,
-  'injectionTemperature': string,
-  'collisions': string,
-  'matchContainer': string,
-  'setTo': string,
-  'returnLid': string,
-  'data': string,
-  'centerOfMass': string,
-  'particleFlowRate': string,
-  'removeDivider': string,
-  'resetDivider': string,
-  'tAvg': string,
-  'tAvgK': string,
-  'massAMU': string,
-  'radiusPm': string,
-  'initialTemperatureK': string,
-  'pressureNoise': string,
-  'scale': string,
-  'oopsTemperatureEmpty': string,
-  'oopsTemperatureOpen': string,
-  'oopsPressureEmpty': string,
-  'oopsPressureLarge': string,
-  'oopsPressureSmall': string,
-  'oopsMaximumTemperature': string
+    'title': string;
+    'nothing': string;
+    'volume': string;
+    'temperature': string;
+    'pressureV': string;
+    'pressureT': string;
+  };
+  'particles': string;
+  'heavy': string;
+  'light': string;
+  'width': string;
+  'stopwatch': string;
+  'collisionCounter': string;
+  'picoseconds': string;
+  'wallCollisions': string;
+  'samplePeriod': string;
+  'kelvin': string;
+  'degreesCelsius': string;
+  'valueUnits': string;
+  'pressure': string;
+  'kilopascals': string;
+  'atmospheres': string;
+  'nanometers': string;
+  'averageSpeed': string;
+  'metersPerSecond': string;
+  'speed': string;
+  'numberOfParticles': string;
+  'kineticEnergy': string;
+  'injectionTemperature': string;
+  'collisions': string;
+  'matchContainer': string;
+  'setTo': string;
+  'returnLid': string;
+  'data': string;
+  'centerOfMass': string;
+  'particleFlowRate': string;
+  'removeDivider': string;
+  'resetDivider': string;
+  'tAvg': string;
+  'tAvgK': string;
+  'massAMU': string;
+  'radiusPm': string;
+  'initialTemperatureK': string;
+  'pressureNoise': string;
+  'scale': string;
+  'oopsTemperatureEmpty': string;
+  'oopsTemperatureOpen': string;
+  'oopsPressureEmpty': string;
+  'oopsPressureLarge': string;
+  'oopsPressureSmall': string;
+  'oopsMaximumTemperature': string;
 };
 
 const gasPropertiesStrings = getStringModule( 'GAS_PROPERTIES' ) as StringsType;
