880c4f83ad42cb13f06e3b408dbd90f3a53b2fa6
phet-dev <phetadmin@gmail.com>
2022-06-03 03:29:21 -0600

    update copyright dates from daily grunt work

--- js/common/model/TemperatureModel.js
+++ js/common/model/TemperatureModel.js
@@ -1,4 +1,4 @@
-// Copyright 2019-2020, University of Colorado Boulder
+// Copyright 2019-2022, University of Colorado Boulder
 
 /**
  * TemperatureModel is a sub-model of IdealGasModel. It is responsible for the T (temperature) component of
