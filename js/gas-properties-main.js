78c2be7293e867cd7531acc9679eb7235376bb04
Michael Kauzmann <michael.kauzmann@colorado.edu>
2022-08-23 09:25:05 -0600

    better tandem name, https://github.com/phetsims/joist/issues/837

--- js/gas-properties-main.js
+++ js/gas-properties-main.js
@@ -26,7 +26,7 @@ const simOptions = {
   preferencesModel: new PreferencesModel( {
     generalOptions: {
       customPreferences: [ {
-        createContent: tandem => new GasPropertiesGlobalOptionsNode( { tandem: tandem } )
+        createContent: tandem => new GasPropertiesGlobalOptionsNode( { tandem: tandem.createTandem( 'gasPropertiesPreferences' ) } )
       } ]
     }
   } ),
