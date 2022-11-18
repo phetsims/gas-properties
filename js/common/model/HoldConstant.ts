f68d2f79250d2d258375545006eefd1d6942bd20
pixelzoom <cmalley@pixelzoom.com>
2022-11-18 16:35:45 -0700

    convert to TypeScript, https://github.com/phetsims/gas-properties/issues/202

--- js/common/model/HoldConstant.ts
+++ js/common/model/HoldConstant.ts
@@ -1,6 +1,5 @@
 // Copyright 2019-2022, University of Colorado Boulder
 
-// @ts-nocheck
 //TODO https://github.com/phetsims/gas-properties/issues/202 change HoldConstant to string union
 /**
  * HoldConstant is an enumeration that identifies which quantity should be held constant when applying
@@ -22,13 +21,14 @@ import EnumerationValue from '../../../../phet-core/js/EnumerationValue.js';
 import gasProperties from '../../gasProperties.js';
 
 export default class HoldConstant extends EnumerationValue {
-  static NOTHING = new HoldConstant();     // change N, T, or V => change P
-  static VOLUME = new HoldConstant();      // change N or T => change P
-  static TEMPERATURE = new HoldConstant(); // change N or V => change P
-  static PRESSURE_V = new HoldConstant();  // change N or T => change V
-  static PRESSURE_T = new HoldConstant();  // change N or V => change T
+  
+  public static readonly NOTHING = new HoldConstant();     // change N, T, or V => change P
+  public static readonly VOLUME = new HoldConstant();      // change N or T => change P
+  public static readonly TEMPERATURE = new HoldConstant(); // change N or V => change P
+  public static readonly PRESSURE_V = new HoldConstant();  // change N or T => change V
+  public static readonly PRESSURE_T = new HoldConstant();  // change N or V => change T
 
-  static enumeration = new Enumeration( HoldConstant );
+  public static readonly enumeration = new Enumeration( HoldConstant );
 }
 
 gasProperties.register( 'HoldConstant', HoldConstant );
--- js/common/model/ParticleType.ts
+++ js/common/model/ParticleType.ts
@@ -1,6 +1,5 @@
 // Copyright 2018-2022, University of Colorado Boulder
 
-// @ts-nocheck
 //TODO https://github.com/phetsims/gas-properties/issues/202 change ParticleType to string union
 /**
  * ParticleType is an enumeration for particle types in the 'Ideal', 'Explore', and 'Energy' screens.
@@ -13,10 +12,11 @@ import EnumerationValue from '../../../../phet-core/js/EnumerationValue.js';
 import gasProperties from '../../gasProperties.js';
 
 export default class ParticleType extends EnumerationValue {
-  static HEAVY = new ParticleType();
-  static LIGHT = new ParticleType();
 
-  static enumeration = new Enumeration( ParticleType );
+  public static readonly HEAVY = new ParticleType();
+  public static readonly LIGHT = new ParticleType();
+
+  public static readonly enumeration = new Enumeration( ParticleType );
 }
 
 gasProperties.register( 'ParticleType', ParticleType );
