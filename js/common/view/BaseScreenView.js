82b544b11d09503f48eb4415715bd6dd91fbd0ce
pixelzoom <cmalley@pixelzoom.com>
2022-11-19 15:21:12 -0700

    // @ts-nocheck

--- js/common/view/BaseScreenView.js
+++ js/common/view/BaseScreenView.js
@@ -1,5 +1,6 @@
 // Copyright 2019-2022, University of Colorado Boulder
 
+// @ts-nocheck
 /**
  * BaseScreenView is the base class for all ScreenViews in this sim.
  *
--- js/common/view/IdealGasLawScreenView.js
+++ js/common/view/IdealGasLawScreenView.js
@@ -1,5 +1,6 @@
 // Copyright 2018-2022, University of Colorado Boulder
 
+// @ts-nocheck
 /**
  * IdealGasLawScreenView is the ScreenView base class for screens that are based on the Ideal Gas Law.
  *
