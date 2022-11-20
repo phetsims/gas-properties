d5fc12e9242f6500b650190ba62731ddacd0f127
pixelzoom <cmalley@pixelzoom.com>
2022-11-20 10:12:56 -0700

    // @ts-nocheck

--- js/energy/model/AverageSpeedModel.js
+++ js/energy/model/AverageSpeedModel.js
@@ -1,5 +1,6 @@
 // Copyright 2019-2022, University of Colorado Boulder
 
+// @ts-nocheck
 /**
  * AverageSpeedModel is a sub-model in the Energy screen, responsible for data that is displayed in the
  * Average Speed accordion box.
