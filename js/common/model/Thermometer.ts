db5085e5d5c00b62b2fdfc672d63e2e7865865ce
pixelzoom <cmalley@pixelzoom.com>
2022-11-23 10:52:59 -0700

    convert to TypeScript, https://github.com/phetsims/gas-properties/issues/202

--- js/common/model/Thermometer.ts
+++ js/common/model/Thermometer.ts
@@ -1,6 +1,5 @@
 // Copyright 2018-2022, University of Colorado Boulder
 
-// @ts-nocheck
 /**
  * Thermometer is the model for the thermometer. It is responsible for determining what units will be used to present
  * the temperature, and for deriving pressure in those units.
@@ -11,9 +10,11 @@
 import DerivedProperty from '../../../../axon/js/DerivedProperty.js';
 import EnumerationProperty from '../../../../axon/js/EnumerationProperty.js';
 import Property from '../../../../axon/js/Property.js';
+import TReadOnlyProperty from '../../../../axon/js/TReadOnlyProperty.js';
 import Range from '../../../../dot/js/Range.js';
-import merge from '../../../../phet-core/js/merge.js';
-import Tandem from '../../../../tandem/js/Tandem.js';
+import optionize from '../../../../phet-core/js/optionize.js';
+import PickRequired from '../../../../phet-core/js/types/PickRequired.js';
+import { PhetioObjectOptions } from '../../../../tandem/js/PhetioObject.js';
 import NullableIO from '../../../../tandem/js/types/NullableIO.js';
 import NumberIO from '../../../../tandem/js/types/NumberIO.js';
 import gasProperties from '../../gasProperties.js';
@@ -22,53 +23,54 @@ import ThermometerUnits from './ThermometerUnits.js';
 // constants
 const DEFAULT_RANGE = new Range( 0, 1000 ); // in K
 
+type SelfOptions = {
+  range?: Range;
+};
+
+type ThermometerOptions = SelfOptions & PickRequired<PhetioObjectOptions, 'tandem'>;
+
 export default class Thermometer {
 
-  /**
-   * @param {Property.<number|null>} temperatureKelvinProperty - temperature in the container, in K
-   * @param {Object} [options]
-   */
-  constructor( temperatureKelvinProperty, options ) {
-    assert && assert( temperatureKelvinProperty instanceof Property,
-      `invalid temperatureKelvinProperty: ${temperatureKelvinProperty}` );
+  // range of thermometer display, in K. temperatureProperty is expected to exceed this.
+  public readonly range: Range;
 
-    options = merge( {
-      range: DEFAULT_RANGE,
+  // temperature in the container, in K. null when the container is empty.
+  public readonly temperatureKelvinProperty: Property<number | null>;
 
-      // phet-io
-      tandem: Tandem.REQUIRED
-    }, options );
+  // temperature in the container, in Celsius. null when the container is empty.
+  public readonly temperatureCelsiusProperty: TReadOnlyProperty<number | null>;
 
-    // @public {Range} range of thermometer display, in K. temperatureProperty is expected to exceed this.
+  // temperature units displayed by the thermometer
+  public readonly unitsProperty: EnumerationProperty<ThermometerUnits>;
+
+  public constructor( temperatureKelvinProperty: Property<number | null>, providedOptions: ThermometerOptions ) {
+
+    const options = optionize<ThermometerOptions, SelfOptions>()( {
+
+      // SelfOptions
+      range: DEFAULT_RANGE
+    }, providedOptions );
+
     this.range = options.range;
 
-    // @public temperature in the container, in K
-    // Value is null when the container is empty.
     this.temperatureKelvinProperty = temperatureKelvinProperty;
 
-    // @public {Property.<number|null>} temperature in the container, in Celsius.
-    // Value is null when the container is empty.
     this.temperatureCelsiusProperty = new DerivedProperty( [ this.temperatureKelvinProperty ],
       temperatureKelvin => ( temperatureKelvin === null ) ? null : temperatureKelvin - 273.15, {
         units: '\u00B0C',
-        isValidValue: value => ( value === null || ( typeof value === 'number' && value !== 0 ) ),
+        isValidValue: value => ( value === null || value !== 0 ),
         phetioValueType: NullableIO( NumberIO ),
         tandem: options.tandem.createTandem( 'temperatureCelsiusProperty' ),
         phetioDocumentation: 'temperature in degrees C'
       } );
 
-    // @public temperature units displayed by the thermometer
     this.unitsProperty = new EnumerationProperty( ThermometerUnits.KELVIN, {
       tandem: options.tandem.createTandem( 'unitsProperty' ),
       phetioDocumentation: 'units displayed by the thermometer'
     } );
   }
 
-  /**
-   * Resets the thermometer.
-   * @public
-   */
-  reset() {
+  public reset(): void {
     this.unitsProperty.reset();
   }
 }
