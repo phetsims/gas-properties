2e9be42fd8eea2c81ee7135817289a588e4453a4
pixelzoom <cmalley@pixelzoom.com>
2022-11-23 11:33:53 -0700

    convert to TypeScript, https://github.com/phetsims/gas-properties/issues/202

--- js/common/model/ParticleUtils.ts
+++ js/common/model/ParticleUtils.ts
@@ -1,13 +1,12 @@
 // Copyright 2019-2020, University of Colorado Boulder
 
-// @ts-nocheck
 /**
  * ParticleUtils is a set of utility methods related to collections of Particles.
  *
  * @author Chris Malley (PixelZoom, Inc.)
  */
 
-import NumberProperty from '../../../../axon/js/NumberProperty.js';
+import Property from '../../../../axon/js/Property.js';
 import Bounds2 from '../../../../dot/js/Bounds2.js';
 import gasProperties from '../../gasProperties.js';
 import GasPropertiesQueryParameters from '../GasPropertiesQueryParameters.js';
@@ -18,13 +17,11 @@ const ParticleUtils = {
 
   /**
    * Steps a collection of particles.
-   * @param {Particle[]} particles
-   * @param {number} dt - time step in ps
-   * @public
+   * @param particles
+   * @param dt - time step in ps
    */
-  stepParticles( particles, dt ) {
-    assert && assert( Array.isArray( particles ), `invalid particles: ${particles}` );
-    assert && assert( typeof dt === 'number' && dt > 0, `invalid dt: ${dt}` );
+  stepParticles( particles: Particle[], dt: number ): void {
+    assert && assert( dt > 0, `invalid dt: ${dt}` );
 
     for ( let i = particles.length - 1; i >= 0; i-- ) {
       particles[ i ].step( dt );
@@ -33,13 +30,8 @@ const ParticleUtils = {
 
   /**
    * Removes a particle from an array and disposes it.
-   * @param {Particle} particle
-   * @param {Particle[]} particles
-   * @public
    */
-  removeParticle: function( particle, particles ) {
-    assert && assert( particle instanceof Particle, `invalid particle: ${particle}` );
-    assert && assert( Array.isArray( particles ), `invalid particles: ${particles}` );
+  removeParticle: function( particle: Particle, particles: Particle[] ): void {
 
     const index = particles.indexOf( particle );
     assert && assert( index !== -1, 'particle not found' );
@@ -50,14 +42,10 @@ const ParticleUtils = {
 
   /**
    * Removes the last n particles from an array and disposes them.
-   * @param {number} n
-   * @param {Particle[]} particles
-   * @public
    */
-  removeLastParticles: function( n, particles ) {
+  removeLastParticles: function( n: number, particles: Particle[] ): void {
     assert && assert( n <= particles.length,
       `attempted to remove ${n} particles, but we only have ${particles.length} particles` );
-    assert && assert( Array.isArray( particles ), `invalid particles: ${particles}` );
 
     for ( let i = 0; i < n; i++ ) {
       ParticleUtils.removeParticle( particles[ particles.length - 1 ], particles );
@@ -66,24 +54,15 @@ const ParticleUtils = {
 
   /**
    * Removes and disposes an entire collection of particles.
-   * @param {Particle[]} particles
-   * @public
    */
-  removeAllParticles: function( particles ) {
-    assert && assert( Array.isArray( particles ), `invalid particles: ${particles}` );
-
+  removeAllParticles: function( particles: Particle[] ): void {
     ParticleUtils.removeLastParticles( particles.length, particles );
   },
 
   /**
    * Removes particles that are out of bounds and disposes them.
-   * @param {Particle[]} particles
-   * @param {Bounds2} bounds
-   * @public
    */
-  removeParticlesOutOfBounds: function( particles, bounds ) {
-    assert && assert( Array.isArray( particles ), `invalid particles: ${particles}` );
-    assert && assert( bounds instanceof Bounds2, `invalid bounds: ${bounds}` );
+  removeParticlesOutOfBounds: function( particles: Particle[], bounds: Bounds2 ): void {
 
     // Iterate backwards, since we're modifying the array, so we don't skip any particles.
     for ( let i = particles.length - 1; i >= 0; i-- ) {
@@ -95,13 +74,11 @@ const ParticleUtils = {
 
   /**
    * Redistributes particles in the horizontal dimension.
-   * @param {Particle[]} particles
-   * @param {number} scaleX - amount to scale the position's x component
-   * @public
+   * @param particles
+   * @param scaleX - amount to scale the position's x component
    */
-  redistributeParticles: function( particles, scaleX ) {
-    assert && assert( Array.isArray( particles ), `invalid particles: ${particles}` );
-    assert && assert( typeof scaleX === 'number' && scaleX > 0, `invalid scaleX: ${scaleX}` );
+  redistributeParticles: function( particles: Particle[], scaleX: number ): void {
+    assert && assert( scaleX > 0, `invalid scaleX: ${scaleX}` );
 
     for ( let i = particles.length - 1; i >= 0; i-- ) {
       particles[ i ].position.setX( scaleX * particles[ i ].position.x );
@@ -110,14 +87,11 @@ const ParticleUtils = {
 
   /**
    * Heats or cools a collection of particles.
-   * @param {Particle[]} particles
-   * @param {number} heatCoolFactor - (-1,1), heat=[0,1), cool=(-1,0]
-   * @public
+   * @param particles
+   * @param heatCoolFactor - (-1,1), heat=[0,1), cool=(-1,0]
    */
-  heatCoolParticles: function( particles, heatCoolFactor ) {
-    assert && assert( Array.isArray( particles ), `invalid particles: ${particles}` );
-    assert && assert( typeof heatCoolFactor === 'number' && heatCoolFactor >= -1 && heatCoolFactor <= 1,
-      `invalid heatCoolFactor: ${heatCoolFactor}` );
+  heatCoolParticles: function( particles: Particle[], heatCoolFactor: number ): void {
+    assert && assert( heatCoolFactor >= -1 && heatCoolFactor <= 1, `invalid heatCoolFactor: ${heatCoolFactor}` );
 
     const velocityScale = 1 + heatCoolFactor / GasPropertiesQueryParameters.heatCool;
     for ( let i = particles.length - 1; i >= 0; i-- ) {
@@ -128,26 +102,20 @@ const ParticleUtils = {
   /**
    * Identifies particles that have escaped via the opening in the top of the container, and
    * moves them from insideParticles to outsideParticles.
-   * @param {IdealGasLawContainer} container
-   * @param {NumberProperty} numberOfParticlesProperty - number of particles inside the container
-   * @param {Particle[]} insideParticles - particles inside the container
-   * @param {Particle[]} outsideParticles - particles outside the container
-   * @public
+   * @param container
+   * @param numberOfParticlesProperty - number of particles inside the container
+   * @param insideParticles - particles inside the container
+   * @param outsideParticles - particles outside the container
    */
-  escapeParticles: function( container, numberOfParticlesProperty, insideParticles, outsideParticles ) {
-    assert && assert( container instanceof IdealGasLawContainer, `invalid container: ${container}` );
-    assert && assert( numberOfParticlesProperty instanceof NumberProperty,
-      `invalid numberOfParticlesProperty: ${numberOfParticlesProperty}` );
-    assert && assert( Array.isArray( insideParticles ), `invalid insideParticles: ${insideParticles}` );
-    assert && assert( Array.isArray( outsideParticles ), `invalid outsideParticles: ${outsideParticles}` );
+  escapeParticles: function( container: IdealGasLawContainer, numberOfParticlesProperty: Property<number>,
+                             insideParticles: Particle[], outsideParticles: Particle[] ): void {
 
     // Iterate backwards, since we're modifying the array, so we don't skip any particles.
     for ( let i = insideParticles.length - 1; i >= 0; i-- ) {
       const particle = insideParticles[ i ];
-      assert && assert( particle instanceof Particle, `invalid particle: ${particle}` );
       if ( particle.top > container.top &&
            particle.left > container.getOpeningLeft() &&
-           particle.right < container.openingRight ) {
+           particle.right < container.getOpeningRight() ) {
         insideParticles.splice( insideParticles.indexOf( particle ), 1 );
         numberOfParticlesProperty.value--;
         outsideParticles.push( particle );
@@ -156,13 +124,9 @@ const ParticleUtils = {
   },
 
   /**
-   * Gets the total kinetic energy of a collection of particles.
-   * @param {Particle[]} particles
-   * @returns {number} in AMU * pm^2 / ps^2
-   * @public
+   * Gets the total kinetic energy of a collection of particles, in AMU * pm^2 / ps^2
    */
-  getTotalKineticEnergy: function( particles ) {
-    assert && assert( Array.isArray( particles ), `invalid particles: ${particles}` );
+  getTotalKineticEnergy: function( particles: Particle[] ): number {
 
     let totalKineticEnergy = 0;
     for ( let i = particles.length - 1; i >= 0; i-- ) {
@@ -173,12 +137,9 @@ const ParticleUtils = {
 
   /**
    * Gets the centerX of mass for a collection of particles.
-   * @param {Particle[]} particles
-   * @returns {number|null} null if there are no particles and therefore no center of mass
-   * @public
+   * null if there are no particles and therefore no center of mass
    */
-  getCenterXOfMass: function( particles ) {
-    assert && assert( Array.isArray( particles ), `invalid particles: ${particles}` );
+  getCenterXOfMass: function( particles: Particle[] ): number | null {
 
     let centerXOfMass = null;
     if ( particles.length > 0 ) {
