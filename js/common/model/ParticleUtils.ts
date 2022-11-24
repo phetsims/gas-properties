eee85f4725fb9f47e57cd864c4f746eb00752931
phet-dev <phetadmin@gmail.com>
2022-11-24 03:54:36 -0700

    update copyright dates from daily grunt work

--- js/common/model/ParticleUtils.ts
+++ js/common/model/ParticleUtils.ts
@@ -1,4 +1,4 @@
-// Copyright 2019-2020, University of Colorado Boulder
+// Copyright 2019-2022, University of Colorado Boulder
 
 /**
  * ParticleUtils is a set of utility methods related to collections of Particles.
