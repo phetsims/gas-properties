dc56b9ba233a2f545ad5433d7084bb0788c08afb
pixelzoom <cmalley@pixelzoom.com>
2022-11-25 13:23:46 -0700

    // @ts-nocheck

--- js/common/view/FixedWidthNode.js
+++ js/common/view/FixedWidthNode.js
@@ -1,5 +1,6 @@
 // Copyright 2019-2022, University of Colorado Boulder
 
+// @ts-nocheck
 //TODO https://github.com/phetsims/gas-properties/issues/201 replace FixedWidthNode with layout options
 /**
  * FixedWidthNode is a Node that has a fixed width. This is used for the content in Panels and AccordionBoxes.
