924a60d94e4f27b4d781326bcfae952a5103ef9b
pixelzoom <cmalley@pixelzoom.com>
2022-11-25 13:03:24 -0700

    // @ts-nocheck

--- js/common/view/DimensionalArrowsNode.js
+++ js/common/view/DimensionalArrowsNode.js
@@ -1,5 +1,6 @@
 // Copyright 2018-2022, University of Colorado Boulder
 
+// @ts-nocheck
 /**
  * DimensionalArrowsNode is a horizontal dimensional arrow. It looks like this, but with solid arrow heads:
  *
