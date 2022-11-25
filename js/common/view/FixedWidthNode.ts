561d654ea62a33cd9454d2480e23c8971fb50889
pixelzoom <cmalley@pixelzoom.com>
2022-11-25 13:29:39 -0700

    convert to TypeScript, https://github.com/phetsims/gas-properties/issues/202

--- js/common/view/FixedWidthNode.ts
+++ js/common/view/FixedWidthNode.ts
@@ -1,6 +1,5 @@
 // Copyright 2019-2022, University of Colorado Boulder
 
-// @ts-nocheck
 //TODO https://github.com/phetsims/gas-properties/issues/201 replace FixedWidthNode with layout options
 /**
  * FixedWidthNode is a Node that has a fixed width. This is used for the content in Panels and AccordionBoxes.
@@ -12,39 +11,41 @@
  * @author Chris Malley (PixelZoom, Inc.)
  */
 
-import merge from '../../../../phet-core/js/merge.js';
-import { HStrut, Node } from '../../../../scenery/js/imports.js';
+import optionize from '../../../../phet-core/js/optionize.js';
+import { HStrut, Node, NodeOptions } from '../../../../scenery/js/imports.js';
 import gasProperties from '../../gasProperties.js';
 
 // constants
-const ALIGN_VALUES = [ 'left', 'right', 'center' ];
+type Align = 'left' | 'right' | 'center';
+
+type SelfOptions = {
+  align?: Align; // horizontal alignment of content in fixedWidth
+};
+
+type FixedWidthNodeOptions = SelfOptions;
 
 export default class FixedWidthNode extends Node {
 
   /**
-   * @param {number} fixedWidth - this Node will be exactly this width
-   * @param {Node} content - Node wrapped by this Node
-   * @param {Object} [options]
+   * @param fixedWidth - this Node will be exactly this width
+   * @param content - Node wrapped by this Node
+   * @param providedOptions
    */
-  constructor( fixedWidth, content, options ) {
-    assert && assert( typeof fixedWidth === 'number' && fixedWidth > 0, `invalid fixedWidth: ${fixedWidth}` );
-    assert && assert( content instanceof Node, `invalid content: ${content}` );
-
-    options = merge( {
-      align: 'left' // horizontal alignment of content in fixedWidth, see ALIGN_VALUES
-    }, options );
-
-    assert && assert( _.includes( ALIGN_VALUES, options.align ), `invalid align: ${options.align}` );
+  public constructor( fixedWidth: number, content: Node, providedOptions?: FixedWidthNodeOptions ) {
+    assert && assert( fixedWidth > 0, `invalid fixedWidth: ${fixedWidth}` );
 
     // prevents the content from getting narrower than fixedWidth
     const strut = new HStrut( fixedWidth, { pickable: false } );
 
-    assert && assert( options.maxWidth === undefined, 'FixedWidthNode sets maxWidth' );
-    assert && assert( !options.children, 'FixedWidthNode sets children' );
-    options = merge( {
+    const options = optionize<FixedWidthNodeOptions, SelfOptions, NodeOptions>()( {
+
+      // SelfOptions
+      align: 'left',
+
+      // NodeOptions
       maxWidth: fixedWidth, // prevents the content from getting wider than fixedWidth
       children: [ strut, content ]
-    }, options );
+    }, providedOptions );
 
     // align content in fixedWidth
     if ( options.align === 'left' ) {
