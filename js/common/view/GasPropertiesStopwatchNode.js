3d46428b1ad6ec8be17dc3b0f539b59820a88a39
pixelzoom <cmalley@pixelzoom.com>
2022-11-25 14:07:04 -0700

    // @ts-nocheck

--- js/common/view/GasPropertiesStopwatchNode.js
+++ js/common/view/GasPropertiesStopwatchNode.js
@@ -1,5 +1,6 @@
 // Copyright 2019-2022, University of Colorado Boulder
 
+// @ts-nocheck
 /**
  * GasPropertiesStopwatchNode is a specialization of StopwatchNode for this sim, a digital stopwatch.
  *
