96f18d282d20fc76c0b1fb468b60ff75ad92917d
Chris Malley <cmalley@pixelzoom.com>
2022-11-28 15:56:49 -0700

    // @ts-nocheck

--- js/common/view/GasPropertiesHeaterCoolerNode.js
+++ js/common/view/GasPropertiesHeaterCoolerNode.js
@@ -1,5 +1,6 @@
 // Copyright 2018-2022, University of Colorado Boulder
 
+// @ts-nocheck
 /**
  * GasPropertiesHeaterCoolerNode is a specialization of HeaterCoolerNode for this sim.  Responsibilities include:
  *
