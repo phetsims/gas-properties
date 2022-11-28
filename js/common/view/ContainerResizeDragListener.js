f0de637f726c832169fd247374ee545c836451dc
Chris Malley <cmalley@pixelzoom.com>
2022-11-28 15:50:36 -0700

    // @ts-nocheck

--- js/common/view/ContainerResizeDragListener.js
+++ js/common/view/ContainerResizeDragListener.js
@@ -1,5 +1,6 @@
 // Copyright 2019-2022, University of Colorado Boulder
 
+// @ts-nocheck
 /**
  * ContainerResizeDragListener is the drag listener for resizing the container by changing its width.
  *
