a023554e4e6f796fbaf65c5ec9b4ab653bc988c0
Chris Malley <cmalley@pixelzoom.com>
2022-11-28 14:49:27 -0700

    // @ts-nocheck

--- js/common/view/ParticleTypeRadioButtonGroup.js
+++ js/common/view/ParticleTypeRadioButtonGroup.js
@@ -1,5 +1,6 @@
 // Copyright 2018-2022, University of Colorado Boulder
 
+// @ts-nocheck
 /**
  * ParticleTypeRadioButtonGroup is a pair of radio buttons for selecting between heavy and light particle types.
  *
