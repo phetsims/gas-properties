8b7331250cdaaaf9a3813511c30915bc571d7dfe
phet-dev <phetadmin@gmail.com>
2023-06-01 03:29:21 -0600

    update copyright dates from daily grunt work

--- js/diffusion/model/DiffusionModel.ts
+++ js/diffusion/model/DiffusionModel.ts
@@ -1,4 +1,4 @@
-// Copyright 2018-2022, University of Colorado Boulder
+// Copyright 2018-2023, University of Colorado Boulder
 
 /**
  * DiffusionModel is the top-level model for the 'Diffusion' screen.
