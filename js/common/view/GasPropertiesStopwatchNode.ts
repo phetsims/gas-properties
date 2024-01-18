7a52c48ab2892644dc8afb426b523d47d621a56d
phet-dev <phetadmin@gmail.com>
2024-01-18 03:13:05 -0700

    update copyright dates from daily grunt work

--- js/common/view/GasPropertiesStopwatchNode.ts
+++ js/common/view/GasPropertiesStopwatchNode.ts
@@ -1,4 +1,4 @@
-// Copyright 2019-2023, University of Colorado Boulder
+// Copyright 2019-2024, University of Colorado Boulder
 
 /**
  * GasPropertiesStopwatchNode is a specialization of StopwatchNode for this sim, a digital stopwatch.
