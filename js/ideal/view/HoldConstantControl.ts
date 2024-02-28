515126fb707dbad65921b854749d0d1095b31753
phet-dev <phetadmin@gmail.com>
2024-02-28 03:17:08 -0700

    update copyright dates from daily grunt work

--- js/ideal/view/HoldConstantControl.ts
+++ js/ideal/view/HoldConstantControl.ts
@@ -1,4 +1,4 @@
-// Copyright 2018-2023, University of Colorado Boulder
+// Copyright 2018-2024, University of Colorado Boulder
 
 /**
  * HoldConstantControl is the control for selecting which quantity should be held constant.
