8a346a040717e2b9ae500018336d4fcd0aa1657b
pixelzoom <cmalley@pixelzoom.com>
2024-03-29 11:49:40 -0600

    add mipmap support, https://github.com/phetsims/gas-properties/issues/219

--- js/common/view/ParticlesNode.ts
+++ js/common/view/ParticlesNode.ts
@@ -16,9 +16,14 @@ import gasProperties from '../../gasProperties.js';
 import Particle from '../model/Particle.js';
 import ParticleNode from './ParticleNode.js';
 
-// constants
-const IMAGE_SCALE = 2; // scale images to improve quality, see https://github.com/phetsims/gas-properties/issues/55
+// Padding around the HTMLCanvasElement created using toCanvas, so we can reliably center it.
 const IMAGE_PADDING = 2;
+
+// Scale particles up by this much to improve resolution.
+const PARTICLE_RESOLUTION_SCALE = 8;
+
+// Inverse scale to apply when rendering particles.
+const PARTICLE_INVERSE_SCALE = 1 / PARTICLE_RESOLUTION_SCALE;
 
 export default class ParticlesNode extends Sprites {
 
@@ -41,7 +46,10 @@ export default class ParticlesNode extends Sprites {
     // a Sprite for each Particle array, indexed the same as particleArrays and imageProperties
     const sprites: Sprite[] = imageProperties.map( imageProperty => {
       const imageToSpriteImage = ( image: HTMLCanvasElement ) => {
-        return new SpriteImage( image, new Vector2( image.width / 2, image.height / 2 ) );
+        return new SpriteImage( image, new Vector2( image.width / 2, image.height / 2 ), {
+          mipmap: true, // To make particles crisper when zooming in using pan-and-zoom feature.
+          mipmapBias: -0.7 // Use a negative value to increase the displayed resolution. See Imageable.setMipmapBias.
+        } );
       };
       const sprite = new Sprite( imageToSpriteImage( imageProperty.value ) );
       imageProperty.lazyLink( image => {
@@ -90,7 +98,7 @@ export default class ParticlesNode extends Sprites {
         if ( this.spriteInstances.length === spriteInstancesIndex ) {
           const newInstance = SpriteInstance.pool.fetch();
           newInstance.alpha = 1;
-          newInstance.matrix.setToAffine( 1 / IMAGE_SCALE, 0, 0, 0, 1 / IMAGE_SCALE, 0 );
+          newInstance.matrix.setToAffine( PARTICLE_INVERSE_SCALE, 0, 0, 0, PARTICLE_INVERSE_SCALE, 0 );
           this.spriteInstances.push( newInstance );
         }
 
@@ -120,7 +128,7 @@ export default class ParticlesNode extends Sprites {
 
     // Create a particle Node, scaled up to improve quality.
     const particleNode = new ParticleNode( particle, modelViewTransform );
-    particleNode.setScaleMagnitude( IMAGE_SCALE, IMAGE_SCALE );
+    particleNode.setScaleMagnitude( PARTICLE_RESOLUTION_SCALE );
 
     // Provide our own integer width and height, so that we can reliably center the image
     const canvasWidth = Math.ceil( particleNode.width + IMAGE_PADDING );
