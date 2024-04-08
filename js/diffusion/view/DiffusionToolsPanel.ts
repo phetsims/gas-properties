ed05f208ddbfd607831824a440e647aba579cb87
pixelzoom <cmalley@pixelzoom.com>
2024-04-08 12:09:12 -0600

    doc

--- js/diffusion/view/DiffusionToolsPanel.ts
+++ js/diffusion/view/DiffusionToolsPanel.ts
@@ -1,7 +1,7 @@
 // Copyright 2018-2023, University of Colorado Boulder
 
 /**
- * DiffusionToolsPanel is the panel for setting the visibility of various things in the Diffusion screen.
+ * DiffusionToolsPanel is the panel for setting the visibility of tools in the Diffusion screen.
  *
  * @author Chris Malley (PixelZoom, Inc.)
  */
