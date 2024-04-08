bce8fbb2c5a61df3388005a4e501701ecd67551f
pixelzoom <cmalley@pixelzoom.com>
2024-04-08 12:12:13 -0600

    pdomPlayAreaNode.pdomOrder and pdomControlAreaNode.pdomOrder for Energy screen, https://github.com/phetsims/gas-properties/issues/213

--- js/energy/view/EnergyScreenView.ts
+++ js/energy/view/EnergyScreenView.ts
@@ -128,27 +128,25 @@ export default class EnergyScreenView extends IdealGasLawScreenView {
 
     // Play Area focus order, see https://github.com/phetsims/gas-properties/issues/213.
     this.pdomPlayAreaNode.pdomOrder = [
-      //TODO https://github.com/phetsims/gas-properties/issues/213
+      this.heavyBicyclePumpNode,
+      this.lightBicyclePumpNode,
+      this.particleTypeRadioButtonGroup,
+      particlesAccordionBox,
+      injectionTemperatureAccordionBox,
+      this.eraseParticlesButton,
+      this.heaterCoolerNode,
+      this.containerNode,
+      this.returnLidButton,
+      this.stopwatchNode,
+      leftPanels
     ];
 
     // Control Area focus order, see https://github.com/phetsims/gas-properties/issues/213.
     this.pdomControlAreaNode.pdomOrder = [
-      //TODO https://github.com/phetsims/gas-properties/issues/213
-      leftPanels,
-      this.stopwatchNode,
-      this.returnLidButton,
-      this.containerNode,
       this.thermometerNode,
       this.pressureGaugeNode,
-      this.timeControlNode,
-      this.heaterCoolerNode,
-      this.eraseParticlesButton,
-      this.heavyBicyclePumpNode,
-      this.lightBicyclePumpNode,
-      this.particleTypeRadioButtonGroup,
       toolsPanel,
-      particlesAccordionBox,
-      injectionTemperatureAccordionBox,
+      this.timeControlNode,
       this.resetAllButton
     ];
   }
