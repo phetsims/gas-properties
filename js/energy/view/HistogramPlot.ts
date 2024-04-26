4b68df13a19c68c6e5d873815aa72cebfab31e9e
pixelzoom <cmalley@pixelzoom.com>
2024-04-25 18:03:43 -0600

    doc

--- js/energy/view/HistogramPlot.ts
+++ js/energy/view/HistogramPlot.ts
@@ -1,8 +1,8 @@
 // Copyright 2024, University of Colorado Boulder
 
 /**
- * HistogramPlot plots a histogram in the familiar 'bars' style, or as a set of connected lines. The data set is the
- * set of y values for an ordered set of bins.
+ * HistogramPlot plots a histogram in the familiar 'bars' style, or as a set of connected lines. The data set consists
+ * of the y values for an ordered set of bins.
  *
  * @author Chris Malley (PixelZoom, Inc.)
  */
