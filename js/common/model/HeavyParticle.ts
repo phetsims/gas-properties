8afd61f8f37e9203ba47170cf8b416825e46c123
phet-dev <phetadmin@gmail.com>
2024-05-02 03:17:24 -0600

    update copyright dates from daily grunt work

--- js/common/model/HeavyParticle.ts
+++ js/common/model/HeavyParticle.ts
@@ -1,4 +1,4 @@
-// Copyright 2019-2022, University of Colorado Boulder
+// Copyright 2019-2024, University of Colorado Boulder
 
 /**
  * HeavyParticle is the model for 'heavy' particles, as they are named in the design document.
--- js/ideal/model/IdealModel.ts
+++ js/ideal/model/IdealModel.ts
@@ -1,4 +1,4 @@
-// Copyright 2019-2022, University of Colorado Boulder
+// Copyright 2019-2024, University of Colorado Boulder
 
 /**
  * IdealModel is the top-level model for the 'Ideal' screen.
