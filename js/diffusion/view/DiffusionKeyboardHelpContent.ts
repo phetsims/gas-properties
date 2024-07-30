5cfe77d3a5d292ecce28bb624dd14cca081a5a11
pixelzoom <cmalley@pixelzoom.com>
2024-07-30 16:57:14 -0400

    includeSmallerStepsRow, https://github.com/phetsims/gas-properties/issues/290

--- js/diffusion/view/DiffusionKeyboardHelpContent.ts
+++ js/diffusion/view/DiffusionKeyboardHelpContent.ts
@@ -25,6 +25,7 @@ export default class DiffusionKeyboardHelpContent extends TwoColumnKeyboardHelpC
 
       // Spinner Controls
       new SpinnerControlsKeyboardHelpSection( {
+        includeSmallerStepsRow: false,
         includeLargerStepsRow: false
       } )
     ];
