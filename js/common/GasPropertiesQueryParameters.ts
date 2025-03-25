46dd53a716aa83e1b87911925b0e5c3dfc36e8db
Michael Kauzmann <michael.kauzmann@colorado.edu>
2025-03-25 16:33:08 -0600

    runnable *QueryParameters.ts files to use QueryStringMachineModule, https://github.com/phetsims/query-string-machine/issues/54

--- js/common/GasPropertiesQueryParameters.ts
+++ js/common/GasPropertiesQueryParameters.ts
@@ -8,6 +8,7 @@
  */
 
 import logGlobal from '../../../phet-core/js/logGlobal.js';
+import { QueryStringMachine } from '../../../query-string-machine/js/QueryStringMachineModule.js';
 import gasProperties from '../gasProperties.js';
 
 const GasPropertiesQueryParameters = QueryStringMachine.getAll( {
